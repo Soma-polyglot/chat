6fba887ffe31c6c378c1aab97c2cc57c06f1610e
Soma Senga <senga.s.0801@gmail.com>
2019-11-05 17:38:10 +0900

    非同期

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,48 @@
 $(function(){
+  function buildHTML(message){
+    var image = message.image ? `<img src= ${message.image}>` : "" ;
+      var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                    ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">
+                    ${message.content}
+                    </p>
+                    ${image}
+                  </div>
+                </div>`
+      return html;
+    }
+  
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formdata = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,  
+      type: 'POST',  
+      data: formdata,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('.submit').prop('disabled', false);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
+    })
+
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+    return false;
+  })
 })
